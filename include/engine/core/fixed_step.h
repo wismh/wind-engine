cd32b8f31b6b0ab65a9f6bbb6a102172afd9ae64
wismh <68060501+wismh@users.noreply.github.com>
2023-02-24 10:15:00 +0300

    wind-4 feat: define fixed timestep clock interfaces and types

---
+++ include/engine/core/fixed_step.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <engine/core/application_state.h>
+#include <engine/core/time.h>
+
+namespace engine {
+
+class FixedStepClock {
+public:
+    FixedStepClock(Time& time, const ApplicationState& app_state);
+
+    // Clamps wall dt, runs 0..MAX_FIXED_STEPS sim ticks when not paused, returns the step count.
+    // Does not call IGame. Leftover accumulator is discarded if the cap is hit.
+    [[nodiscard]] int advance(float real_dt);
+
+private:
+    Time* time_ = nullptr;
+    const ApplicationState* app_state_ = nullptr;
+};
+
+}
