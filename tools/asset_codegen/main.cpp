d2bd8ec37ddafc0e46513afa0fdf0f0202dea0b4
wismh <68060501+wismh@users.noreply.github.com>
2023-03-29 14:20:00 +0300

    wind-6 feat: add guid asset catalog and codegen assets and build integration

---
+++ tools/asset_codegen/main.cpp
@@ -0,0 +1,18 @@
+#include <engine/resources/meta.h>
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "usage: asset_codegen <assets_dir> <output_dir>\n";
+        return 2;
+    }
+
+    const auto result = engine::codegen_write(argv[1], argv[2]);
+    if (!result) {
+        std::cerr << result.error().message << '\n';
+        return 1;
+    }
+    return 0;
+}
