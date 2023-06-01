5f54038316537cd3478c5876ec91c0d59d7ddaf2
wismh <68060501+wismh@users.noreply.github.com>
2023-06-01 10:15:00 +0300

    wind-10 feat: define fake mixer audio policy interfaces and types

--- include/engine/engine.h
+++ include/engine/engine.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <engine/audio/audio_system.h>
+#include <engine/audio/sound.h>
 #include <engine/core/application_state.h>
 #include <engine/core/fixed_step.h>
 #include <engine/core/host.h>
