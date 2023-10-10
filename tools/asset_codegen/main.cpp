f01b8fd2946bd55d56a516c5bba4c0bee13b114d
wismh <68060501+wismh@users.noreply.github.com>
2023-10-10 14:20:00 +0300

    wind-18 feat: add cooked catalogs with texture settings assets and build integration

--- tools/asset_codegen/main.cpp
+++ tools/asset_codegen/main.cpp
@@ -2,15 +2,19 @@
 #include <engine/resources/meta.h>
 
 #include <iostream>
-#include <string>
+#include <span>
+#include <string_view>
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        std::cerr << "usage: asset_codegen <assets_dir> <output_dir>\n";
+        std::cerr << "usage: asset_codegen <assets_dir> <output_dir> [--engine]\n";
         return 2;
     }
 
-    const auto result = engine::codegen_write(argv[1], argv[2], engine::builtin::reserved());
+    const bool engine_builtins = argc >= 4 && std::string_view(argv[3]) == "--engine";
+    const std::span<const engine::AssetId> reserved =
+            engine_builtins ? std::span<const engine::AssetId>{} : engine::builtin::reserved();
+    const auto result = engine::codegen_write(argv[1], argv[2], reserved);
     if (!result) {
         std::cerr << result.error().message << '\n';
         return 1;
